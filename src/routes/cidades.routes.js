aa15ff8189262e9c210983fdb9526328fd4c5254
Matheus Yuji <matheus.y.shiraishi@gmail.com>
2021-03-03 01:12:25 -0300

    Final commit

--- src/routes/cidades.routes.js
+++ src/routes/cidades.routes.js
@@ -1,7 +1,6 @@
 const router = require("express-promise-router")();
 const cidadesController = require("../controllers/cidades.controller");
 
-router.post("/cidades", cidadesController.createCity);
 router.post("/cidades/recentes", cidadesController.getLast);
 router.get("/cidades/frequencia", cidadesController.getFrequency);
 router.post("/cidades/clima", cidadesController.getWeather);
